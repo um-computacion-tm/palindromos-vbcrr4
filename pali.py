265c99474c525981d49a46c2c5382f01ed3c0a91
vbcrr4 <v.becerra@alumno.um.edu.ar>
2023-04-11 11:25:23 -0300

    correcion de prubas

--- pali.py
+++ pali.py
@@ -4,16 +4,6 @@ def palindrome (phrase):
         return True
     else:
         return False
-
-
-
-'''def palindrome_rec (frase):
-    if len(frase) <= 1:
-        return True
-    if frase [0] == frase [-1]:
-        return palindrome_rec(frase[1:-2])'''
     
-
-
 if __name__=='__main__':
     pass
